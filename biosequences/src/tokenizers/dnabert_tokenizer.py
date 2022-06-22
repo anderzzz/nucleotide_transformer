67e8a0de31bcd145591cfa966cfdae2210883aba
anderzzz <ohrn.anders@gmail.com>
2022-06-22 08:14:13 +0200

    Add fast tokenizer support

--- biosequences/src/tokenizers/dnabert_tokenizer.py
+++ biosequences/src/tokenizers/dnabert_tokenizer.py
@@ -3,6 +3,21 @@
 '''
 from transformers import BertTokenizer, BertTokenizerFast
 
+def _make_phrase_from_(seq, stride, word_length):
+    '''Bla bla
+
+    '''
+    if not isinstance(seq, str):
+        raise TypeError('Encountered sequence that is not string: {}'.format(seq))
+
+    lower_ind = len(seq) - word_length + 1
+    if lower_ind < 1:
+        raise RuntimeError('Sequence too short: {}'.format(seq))
+    chunks = [seq[ind: ind + word_length] for ind in range(0, lower_ind, stride)]
+    phrase = ' '.join(chunks)
+
+    return phrase
+
 class DNABertTokenizer(BertTokenizer):
     '''Bla bla
 
@@ -28,32 +43,40 @@ class DNABertTokenizer(BertTokenizer):
 
     def __call__(self, seq):
         if isinstance(seq, (list,tuple)):
-            text = [self._make_phrase_from_(s) for s in seq]
+            text = [_make_phrase_from_(s, self.stride, self.word_length) for s in seq]
         else:
-            text = self._make_phrase_from_(seq)
+            text = _make_phrase_from_(seq, self.stride, self.word_length)
 
         return super().__call__(text)
-
-    def _make_phrase_from_(self, seq):
-        if not isinstance(seq, str):
-            raise TypeError('Encountered sequence that is not string: {}'.format(seq))
-
-        lower_ind = len(seq) - self.word_length + 1
-        if lower_ind < 1:
-            raise RuntimeError('Sequence too short: {}'.format(seq))
-        chunks = [seq[ind: ind + self.word_length] for ind in range(0, lower_ind, self.stride)]
-        phrase = ' '.join(chunks)
-
-        return phrase
 
 
 class DNABertTokenizerFast(BertTokenizerFast):
     '''Bla bla
 
     '''
-    def __init__(self):
+    def __init__(self, vocab_file,
+                 stride=1, word_length=3,
+                 do_lower_case=True, do_basic_tokenize=True,
+                 unk_token='[UNK]', sep_token='[SEP]', pad_token='[PAD]', cls_token='[CLS]', mask_token='[MASK]'):
         super(DNABertTokenizerFast, self).__init__(
-            'dummy.txt'
+            vocab_file=vocab_file,
+            do_lower_case=do_lower_case,
+            do_basic_tokenize=do_basic_tokenize,
+            tokenize_chinese_chars=False,
+            strip_accents=False,
+            unk_token=unk_token,
+            sep_token=sep_token,
+            pad_token=pad_token,
+            cls_token=cls_token,
+            mask_token=mask_token
         )
+        self.stride = stride
+        self.word_length = word_length
 
-        pass
+    def __call__(self, seq):
+        if isinstance(seq, (list,tuple)):
+            text = [_make_phrase_from_(s, self.stride, self.word_length) for s in seq]
+        else:
+            text = _make_phrase_from_(seq, self.stride, self.word_length)
+
+        return super().__call__(text)
--- biosequences/tests/test_tokenizers_dnabert.py
+++ biosequences/tests/test_tokenizers_dnabert.py
@@ -2,7 +2,7 @@
 
 '''
 from biosequences import dna_nucleotide_alphabet, NucleotideVocabCreator
-from biosequences import DNABertTokenizer
+from biosequences import DNABertTokenizer, DNABertTokenizerFast
 
 SEQ_STR = 'AATGCGT'
 IDS_SEQ_STR = [3,8,19,62,43,32,1]
@@ -25,6 +25,29 @@ def test_tokenize_batch():
     out = tokenizer(SEQ_BATCH)
     assert out.input_ids == IDS_SEQ_BATCH
 
+def test_tokenize_fast_str():
+    dna_vocab = NucleotideVocabCreator(dna_nucleotide_alphabet, do_lower_case=True).generate(3)
+    with open('./tmp_test.txt', 'w') as fout:
+        dna_vocab.save(fout)
+    tokenizer = DNABertTokenizerFast('./tmp_test.txt', word_length=3, stride=1, do_lower_case=True)
+    assert tokenizer.is_fast
+
+    out = tokenizer(SEQ_STR)
+    assert out.input_ids == IDS_SEQ_STR
+
+def test_tokenize_fast_batch():
+    dna_vocab = NucleotideVocabCreator(dna_nucleotide_alphabet, do_lower_case=True).generate(3)
+    with open('./tmp_test.txt', 'w') as fout:
+        dna_vocab.save(fout)
+    tokenizer = DNABertTokenizerFast('./tmp_test.txt', word_length=3, stride=1, do_lower_case=True)
+    assert tokenizer.is_fast
+
+    out = tokenizer(SEQ_BATCH)
+    assert out.input_ids == IDS_SEQ_BATCH
+
+
 if __name__ == '__main__':
     test_tokenize_str()
     test_tokenize_batch()
+    test_tokenize_fast_str()
+    test_tokenize_fast_batch()
