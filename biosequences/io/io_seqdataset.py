802063991c7f321c735e21d42e40e7d99eb68954
anderzzz <ohrn.anders@gmail.com>
2022-07-07 18:32:49 +0200

    Nucleotide processing done for CSV

--- biosequences/io/io_seqdataset.py
+++ biosequences/io/io_seqdataset.py
@@ -3,15 +3,24 @@
 '''
 from pathlib import Path
 import json
-from collection.abc import Iterable
+from collections.abc import Iterable
+import pandas as pd
 
 from torch.utils.data import Dataset
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
 def _read_seq_file_(fp, format_name='genbank'):
-    return [record for record in SeqIO.parse(fp, format_name)]
+    for record in SeqIO.parse(fp, format_name):
+        yield record
 
-class _NucleotideRawSequenceProcessor(object):
+def _read_seq_csv_(fp, index_col=None, key='sequence_chunk', description=''):
+    df = pd.read_csv(fp, index_col=index_col)
+    for index, value in df.iterrows():
+        yield SeqRecord(Seq(value[key]), id=str(index), name=str(fp), description=description)
+
+class NucleotideSequenceProcessor(object):
     '''Bla bla
 
     '''
@@ -19,7 +28,7 @@ class _NucleotideRawSequenceProcessor(object):
                  source_directory=None,
                  source_directory_file_pattern='*',
                  source_files=None,
-                 phrasifier=None
+                 source_file_format='csv'
                 ):
 
         if not source_directory is None:
@@ -37,74 +46,52 @@ class _NucleotideRawSequenceProcessor(object):
         else:
             raise ValueError('Failed to specify raw data source files')
 
-        if phrasifier is None:
-            self.phrasify_ = lambda x:x
-        elif callable(phrasifier):
-            self.phrasify_ = phrasifier
+        if source_file_format == 'csv':
+            self.source_reader = _read_seq_csv_
+            self.source_reader_kwargs = {'index_col' : 0}
+        elif source_file_format in ['genbank', 'fasta']:
+            self.source_reader = _read_seq_file_
+            self.source_reader_kwargs = {'format' : source_file_format}
         else:
-            raise ValueError('The phrasifier has to be callable or None, not of type {}'.format(type(phrasifier)))
+            raise ValueError('Unknown source file format: {}'.format(source_file_format))
 
-class NucleotideSequenceProcessor(_NucleotideRawSequenceProcessor):
-    '''Bla bla
-
-    '''
-    def __init__(self,
-                 source_directory,
-                 input_file_pattern='*.gb',
-                 input_seqio_format='genbank',
-                 attributes_extract=('id', 'name', 'seq'),
-                 phrasifier=None
-                 ):
-
-        super(NucleotideSequenceProcessor, self).__init__(source_directory=source_directory)
-
-        p = Path(source_directory)
-        if not p.is_dir():
-            raise ValueError('File folder {} not found'.format(source_directory))
-
-        self.file_paths = list(p.glob(input_file_pattern))
-        self.input_seqio_format = input_seqio_format
-        self.attributes_extract = attributes_extract
-        if phrasifier is None:
-            self.phrasify_ = lambda x:x
-        elif callable(phrasifier):
-            self.phrasify_ = phrasifier
-        else:
-            raise ValueError('The phrasifier has to be callable or None, not of type {}'.format(type(phrasifier)))
-
-    def convert_to_(self, target_type, **kwargs):
+    def __iter__(self):
         '''Bla bla
 
         '''
-        if target_type == 'json':
-            self._convert_to_json_from_folders_(**kwargs)
-        else:
-            raise ValueError('Unknown target file type: {}'.format(target_type))
-
-    def _convert_to_json_from_folders_(self, out_dir=None, out_file_suffix='json'):
-        '''Bla bla
-
-        '''
-        counter = 0
         for fp in self.file_paths:
-            records = _read_seq_file_(fp, format_name=self.input_seqio_format)
-            for record in records:
-                data = self.extract_(record)
-                with open('{}/{}.{}'.format(out_dir, counter, out_file_suffix), 'w') as f_json:
-                    json.dump(data, indent=4, fp=f_json)
-                    counter += 1
+            for data_package in self.source_reader(fp, **self.source_reader_kwargs):
+                yield data_package
 
-    def extract_(self, record):
+    def save_as_json(self, save_dir=None, save_prefix='', seq_transformer=None, seq_transformer_kwargs={}):
         '''Bla bla
 
         '''
-        ret_data = {}
-        for attribute in self.attributes_extract:
-            val = str(getattr(record, attribute))
-            if attribute == 'seq':
-                val = self.phrasify_(val)
-            ret_data[attribute] = val
-        return ret_data
+        if seq_transformer is None:
+            _transform = lambda x : x
+        elif callable(seq_transformer):
+            _transform = seq_transformer
+        else:
+            raise ValueError('Sequence transformer must be callable')
+
+        for k_record, record in enumerate(self):
+            seq_out = _transform(record.seq.__str__(), **seq_transformer_kwargs)
+            metadata_id = record.id
+            metadata_name = record.name
+            metadata_description = record.description
+            with open('{}/{}{}.json'.format(save_dir, save_prefix, k_record), 'w') as f_json:
+                json.dump({'name': metadata_name,
+                           'id': metadata_id,
+                           'description': metadata_description,
+                           'sequence_data': seq_out},
+                          indent=4, fp=f_json)
+
+from biosequences.utils import Phrasifier
+p = NucleotideSequenceProcessor(source_directory='/Users/andersohrn/PycharmProjects/nucleotide_transformer',
+                                source_directory_file_pattern='test*.csv',
+                                source_file_format='csv')
+pp = Phrasifier(1,3,True)
+p.save_as_json('.',save_prefix='dummy',seq_transformer=pp)
 
 class NucleotideSequenceDataset(Dataset):
     '''Dataset of nucleotide sequences from collection of data in some standard format
--- biosequences/utils/phrase_maker.py
+++ biosequences/utils/phrase_maker.py
@@ -5,9 +5,10 @@ class Phrasifier(object):
     '''Bla bla
 
     '''
-    def __init__(self, stride, word_length):
+    def __init__(self, stride, word_length, upper=False):
         self.stride = stride
         self.word_length = word_length
+        self.upper = upper
 
     def __call__(self, seq):
         if not isinstance(seq, str):
@@ -18,5 +19,7 @@ class Phrasifier(object):
             raise RuntimeError('Sequence too short: {}'.format(seq))
         chunks = [seq[ind: ind + self.word_length] for ind in range(0, lower_ind, self.stride)]
         phrase = ' '.join(chunks)
+        if self.upper:
+            phrase = phrase.upper()
 
         return phrase
