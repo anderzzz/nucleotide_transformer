1054be3f2297efa0d1bad5f514cc18e0bccbb354
anderzzz <ohrn.anders@gmail.com>
2022-07-08 13:25:02 +0200

    Basic but functional LM training script done

--- biosequences/scripts/train_maskedlm.py
+++ biosequences/scripts/train_maskedlm.py
@@ -3,7 +3,7 @@
 '''
 from pathlib import Path
 
-from biosequences.io import NucleotideSequenceProcessor, factory
+from biosequences.io import NucleotideSequenceProcessor
 from biosequences.utils import NucleotideVocabCreator, dna_nucleotide_alphabet, Phrasifier
 from biosequences.datacollators import DataCollatorDNAWithMasking
 
@@ -23,37 +23,65 @@ def sequence_grouper(seqs, chunk_size):
     result['labels'] = result['input_ids'].copy()
     return result
 
-def main(data_folder=None, out_data_folder='/data_out',
+def main(folder_seq_raw=None, seq_raw_format='csv', seq_raw_file_pattern='*.csv', upper_lower='upper',
+         folder_seq_sentence=None, seq_sentence_prefix='',
          vocab_file='vocab.txt', create_vocab=True, word_length_vocab=3, stride=1,
          chunk_size=1000,
-         masking_probability=0.15):
+         masking_probability=0.15,
+         bert_config_kwargs={},
+         folder_training_output=None):
 
     #
-    # Construct Dataset
-    phrasifier = Phrasifier(stride=stride, word_length=word_length_vocab)
-    if gb_data_folder is None:
-        raise ValueError('The folder with Genbank data files must be provided in `gb_data_folder`')
-    dataprocessor = NucleotideSequenceProcessor(gb_data_folder,
-                                                phrasifier=phrasifier,
-                                                attributes_extract=('seq',))
-    dataprocessor.convert_to_('json', out_dir=out_data_folder)
+    # Some argument sanity checks
+    if folder_seq_sentence is None:
+        raise ValueError('The folder for sequence sentence files required')
+
+    if upper_lower == 'upper':
+        do_upper_case = True
+        do_lower_case = False
+    elif upper_lower == 'lower':
+        do_upper_case = False
+        do_lower_case = True
+    else:
+        raise ValueError('The vocabulary is either all upper or all lower, but `upper_lower` of invalid value: {}'.format(upper_lower))
+
+    if folder_training_output is None:
+        folder_training_output_ = folder_seq_sentence
+    else:
+        folder_training_output_ = folder_training_output
+
+    #
+    # Construct sequence sentences from sequence data if such folder is given
+    if not folder_seq_raw is None:
+        dataprocessor = NucleotideSequenceProcessor(source_directory=folder_seq_raw,
+                                                    source_file_format=seq_raw_format,
+                                                    source_directory_file_pattern=seq_raw_file_pattern)
+        phrasifier = Phrasifier(stride=stride,
+                                word_length=word_length_vocab,
+                                do_upper_case=do_upper_case,
+                                do_lower_case=do_lower_case)
+        dataprocessor.save_as_json(save_dir=folder_seq_sentence,
+                                   save_prefix=seq_sentence_prefix,
+                                   seq_transformer=phrasifier)
 
     #
     # Construct the tokenizer
     if create_vocab:
-        dna_vocab = NucleotideVocabCreator(dna_nucleotide_alphabet, do_lower_case=True).generate(word_length_vocab)
-        with open('{}/{}'.format(out_data_folder, vocab_file), 'w') as fout:
+        dna_vocab = NucleotideVocabCreator(alphabet=dna_nucleotide_alphabet,
+                                           do_lower_case=do_lower_case,
+                                           do_upper_case=do_upper_case).generate(word_length_vocab)
+        with open('{}/{}'.format(folder_seq_sentence, vocab_file), 'w') as fout:
             dna_vocab.save(fout)
-    tokenizer = BertTokenizer(vocab_file='{}/{}'.format(out_data_folder, vocab_file))
+    tokenizer = BertTokenizer(vocab_file='{}/{}'.format(folder_seq_sentence, vocab_file), do_lower_case=do_lower_case)
 
     #
     # Tokenize the dataset and make compatible with Huggingface data loaders
-    json_files = Path(out_data_folder).glob('*.json')
+    json_files = Path(folder_seq_sentence).glob('{}*.json'.format(seq_sentence_prefix))
     json_files = ['{}'.format(x.resolve()) for x in json_files]
     seq_dataset = load_dataset('json',
                                data_files=json_files)
     tokenized_dataset = seq_dataset.map(
-        lambda x: tokenizer(x['seq']), batched=True, remove_columns=['seq']
+        lambda x: tokenizer(x['seq']), batched=True, remove_columns=['seq', 'id', 'name', 'description']
     )
     lm_dataset = tokenized_dataset.map(
         sequence_grouper,
@@ -74,13 +102,13 @@ def main(data_folder=None, out_data_folder='/data_out',
 
     #
     # Configure the model for masked language modelling. This is appropriate for training the Bert encoder
-    config = BertConfig()
+    config = BertConfig(**bert_config_kwargs)
     model = BertForMaskedLM(config=config)
 
     #
     # Set up trainer
     training_args = TrainingArguments(
-        output_dir=out_data_folder,
+        output_dir=folder_training_output_,
         overwrite_output_dir=False,
         num_train_epochs=1,
         per_gpu_train_batch_size=64,
@@ -100,6 +128,7 @@ def main(data_folder=None, out_data_folder='/data_out',
     trainer.train()
 
 
-if __name__ == '__main__':
-    main(gb_data_folder='/Users/andersohrn/PycharmProjects/nucleotide_transformer/biosequences/scripts/data',
-         out_data_folder='/Users/andersohrn/PycharmProjects/nucleotide_transformer/biosequences/scripts/data_out')
+#if __name__ == '__main__':
+#    main(
+#        folder_seq_raw=
+#    )
--- biosequences/tests/test_scripts_train_maskedlm.py
+++ biosequences/tests/test_scripts_train_maskedlm.py
@@ -3,4 +3,12 @@
 '''
 from biosequences.scripts.train_maskedlm import main
 
-main()
+TEST_DATA = './tests_data/'
+
+main(folder_seq_raw=TEST_DATA, seq_raw_format='csv', seq_raw_file_pattern='test_xaa.csv', upper_lower='upper',
+     folder_seq_sentence=TEST_DATA, seq_sentence_prefix='test_maskedlm_',
+     vocab_file='vocab.txt', create_vocab=True, word_length_vocab=3, stride=1,
+     chunk_size=100,
+     masking_probability=0.15,
+     bert_config_kwargs={},
+     folder_training_output=TEST_DATA)
